db2c35261ea4c79e560941c0f117e7eb1ff46df4
ART_List <artbdarm9@mail.ru>
2021-01-20 16:06:58 +0300

    added code

--- T-Converter/ViewController.swift
+++ T-Converter/ViewController.swift
@@ -12,15 +12,31 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var celcius: UILabel!
     @IBOutlet weak var fahrenheit: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet{
+            slider.minimumValue = 0
+            slider.maximumValue = 100
+            slider.value = 0
+        }
+    }
     
     
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func converter(_ sender: UISlider) {
+        
+        let temperatureCel = Int(round(sender.value))
+        celcius.text = "\(temperatureCel)ºC"
+        
+        let temperatureFar = Int(round((sender.value * 9 / 5) + 32))
+        
+        fahrenheit.text = "\(temperatureFar)ºF"
+        
     }
     
 }
